1f10adbb3cd5684ebd16d3e84755a6a0b094d0a8
Ahmed <ramerram@gmail.com>
2017-08-15 22:10:58 +0600

    убрал пробелы

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,10 +1,10 @@
-$(document).ready(function () {
-	$('.tickbox').click(function() {
-	  $(this).find('.icon-tick').toggle();
-	});
-	$('.sandwich-menu').click(function() {
-	  $(this).toggleClass('active');
-	  $('ul.header-container__nav').toggle(900);
-	});
-    svg4everybody({});
+$(document).ready(function() {
+  $('.tickbox').click(function() {
+    $(this).find('.icon-tick').toggle();
+  });
+  $('.sandwich-menu').click(function() {
+    $(this).toggleClass('active');
+    $('ul.header-container__nav').toggle(900);
+  });
+  svg4everybody({});
 });
